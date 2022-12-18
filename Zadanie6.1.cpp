7057006afc6b36b87f90a6842234f85ba105623d
PiotrZapolski21700 <116441361+PiotrZapolski21700@users.noreply.github.com>
2022-12-18 19:33:41 +0100

    Add files via upload

---
+++ Zadanie6.1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    int n;
+    int count = 0;
+
+    cout << "Podaj liczbe calkowita:";
+    cin >> n;
+
+    for(int i=0; i<=n ; i++){
+        if(i%5==0 && i%3!=0){
+            count++;
+        }
+    }
+
+    cout << "Jest " << count << " liczb nie wiekszych od " << n << ", podzielnych przez 5 ale niepodzielnych przez 3.";
+
+    return 0;
+}
